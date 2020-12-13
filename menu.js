dccf2b27f88a1433645d16e03de71169ab44c03f
VitorPL07 <62154573+VitorPL07@users.noreply.github.com>
2020-12-13 14:04:13 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,3 +1,3 @@
 module.exports = {
-    menu = "*Bem vindo ao Bot Sticker Fds*\r\n\r\nCriador: *_Victor The Programmer_*\r\nʟɪsᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs\r\n\r\n*!sticker*: Transforma a imagem em figurinha\r\n_Coloque esse comando na legenda da figurinha_\r\n\r\n*!stickergif*: Transforma o gif ou vídeo em figurinha animada"
+    menu = "*Bem vindo ao Bot Sticker Fds*\r\n\r\nCriador: *_Victor The Programmer_*\r\n*Lista De Comandos*\r\n\r\n*!sticker*: Transforma a imagem em figurinha\r\n_Coloque esse comando na legenda da figurinha_\r\n\r\n*!stickergif*: Transforma o gif ou vídeo em figurinha animada"
 }
