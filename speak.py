acdf3405592876418ecb7162381f7baf792584c0
VitorPL07 <62154573+VitorPL07@users.noreply.github.com>
2020-12-15 03:11:30 -0300

    Update speak.py

--- speak.py
+++ speak.py
@@ -5,6 +5,6 @@ msg = sys.argv[1]
 msg = msg.replace("!tts ", "")
 
 obj = gtts.gTTS(text = msg, lang = "pt-br", slow = False)
-obj.save("mp3/sound.mp3")
+obj.save("mp3/sound.wav")
 
 print("Pronto")
