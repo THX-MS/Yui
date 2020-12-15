9877ab761b10f833ae5e8b1370c3704f97988f2e
VitorPL07 <62154573+VitorPL07@users.noreply.github.com>
2020-12-15 01:08:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ apt install
 apt install wget -y
 apt install ffmpeg -y
 apt install nodejs -y
+pkg install python -y
 npm i -g cwebp
 npm i -g ytdl 
 npm i
 npm update
 mkdir mp3
 mkdir mp4
-apt-get install python-pip
 pip install gtts
